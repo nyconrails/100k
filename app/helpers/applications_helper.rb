5a880bb32fb54721e83e959e1c79d2b5d59532a2
David Lesches <david@lesches.com>
2012-08-16 17:39:52 -0400

    Heading updates

--- app/helpers/applications_helper.rb
+++ app/helpers/applications_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationsHelper
+
+  def cool_heading(heading)
+    header = heading.strip.split(' ')
+    if header.size == 1
+      heading
+    elsif header.size == 2
+      raw "<span class=\"contrast\">#{header[0]}</span> #{header[1]}"
+    elsif header.size == 3
+      raw "#{header[0]} <span class=\"contrast\">#{header[1]}</span> #{header[2]}"
+    else
+      raw "<span class=\"contrast\">#{header[0]}</span> " << header.each_with_index.map { |a,i| a.to_s if i != 0 }.join(' ')
+    end
+  end
+
 end
