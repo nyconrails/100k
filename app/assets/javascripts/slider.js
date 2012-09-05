2f7f68a9bedff012343f1381954b1ad5dee87451
David Lesches <david@lesches.com>
2012-09-05 12:22:26 -0400

    slider fixes

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -1,6 +1,6 @@
 // Custom slider by NYC Dev Shop for 100kin10
 
-$(document).ready(function() {
+$(window).load(function() {
   if ( $('#custom_slider').length ) {
 
   var sliderActive = false;
@@ -9,24 +9,24 @@ $(document).ready(function() {
   var current_slide_item = 0;
 
   // Initialize
-  $($slider).find('li').not(":first").hide().css('left', '600px');
-  $($slider).find('li').first().addClass('active');
-  $($triggers).find('li').first().addClass('active');
+  $($slider).find('.sliderli').not(":first").hide().css('left', '600px');
+  $($slider).find('.sliderli').first().addClass('active');
+  $($triggers).find('.sliderli').first().addClass('active');
 
   // Functions
   function slide(item) {
 
     current_slide_item = item;
 
-    var current_item = $('.active', $slider);
-    var new_item = $('li', $slider).eq(item);
+    var current_item = $('.sliderli.active', $slider);
+    var new_item = $('.sliderli', $slider).eq(item);
 
     $(current_item).css('z-index', '10');
     $(new_item).css({'z-index': '5', 'left': '0px'}).show();
 
     $(current_item).fadeOut('medium', function() {
       var ind = parseInt( $(new_item).index() );
-      $('li', $triggers).removeClass('active').eq(ind).addClass('active');
+      $('.triggerli', $triggers).removeClass('active').eq(ind).addClass('active');
       $(current_item).css({'z-index': '1', 'left': '600px'}).removeClass('active');
       $(new_item).addClass('active');
     });
@@ -40,7 +40,7 @@ $(document).ready(function() {
   }
 
   // Clicking triggers
-  $('li', $triggers).click(function() {
+  $('.triggerli', $triggers).click(function() {
     var ind = parseInt( $(this).index() );
     stopSlider();
     slide(ind);
@@ -72,18 +72,18 @@ $(document).ready(function() {
     sliderActive = true;
   };
 
-  _V_("my_video_1").ready(function() {
+  _V_("my_video_a").ready(function() {
     var myVideo = this;
 
     myVideo.addEvent("play", stopSlider);
     myVideo.addEvent("ended", startSlider);
   });
 
-  _V_("my_video_2").ready(function() {
-    var myVideo2 = this;
+  _V_("my_video_b").ready(function() {
+    var myVideob = this;
 
-    myVideo2.addEvent("play", stopSlider);
-    myVideo2.addEvent("ended", startSlider);
+    myVideob.addEvent("play", stopSlider);
+    myVideob.addEvent("ended", startSlider);
   });
 
 
