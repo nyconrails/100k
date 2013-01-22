925d4a01ff77602048e106c687b6e753ec494348
David Lesches <david@lesches.com>
2013-01-22 10:48:10 -0500

    Use sendmail

--- config/environment.rb
+++ config/environment.rb
@@ -2,15 +2,15 @@
 require File.expand_path('../application', __FILE__)
 
 
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => 'app1053523@heroku.com',
-  :password       => 'ojwggfsu',
-  :domain         => 'heroku.com'
-}
-ActionMailer::Base.delivery_method = :smtp
+# ActionMailer::Base.smtp_settings = {
+#   :address        => 'smtp.sendgrid.net',
+#   :port           => '587',
+#   :authentication => :plain,
+#   :user_name      => 'app1053523@heroku.com',
+#   :password       => 'ojwggfsu',
+#   :domain         => 'heroku.com'
+# }
+ActionMailer::Base.delivery_method = :sendmail
 
 
 
